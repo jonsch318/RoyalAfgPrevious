24b28f5d44463e2f4c0012954eb7a8a973f745c7
Jonas Schneider <jonas.max.schneider@live.com>
2019-11-10 19:44:38 +0100

    Added Log Model and LogReader class

---
+++ logs/logReader/LogReader/logReader/LogReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace logReader
+{
+    public class LogReader
+    {
+
+        public List<Log> ReadLogs(string path)
+        {
+            List<Log> logs = new List<Log>();
+            using (var reader = new StreamReader(path))
+            {
+                string content = reader.ReadToEnd();
+
+                string timestamp = content.Substring(1,24);
+
+                content.Remove(0,26);
+
+                string level = content.Split(")")[0];
+
+                content = content.Split(")")[1];
+
+                content.Remove(0,2);
+                
+            }
+
+        }  
+
+    }
+}
