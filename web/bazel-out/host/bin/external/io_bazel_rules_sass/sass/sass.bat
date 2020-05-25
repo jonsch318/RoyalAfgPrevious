bac5e6b78d6e324ff5c58df4f8084f80bc25cc7c
Jonas Schneider <jonas.max.schneider@gmail.com>
2020-05-25 18:05:18 +0200

    Added @angular/bazel

---
+++ web/bazel-out/host/bin/external/io_bazel_rules_sass/sass/sass.bat
@@ -0,0 +1,56 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+set RUNFILES_MANIFEST_ONLY=1
+
+rem Usage of rlocation function:
+rem        call :rlocation <runfile_path> <abs_path>
+rem        The rlocation function maps the given <runfile_path> to its absolute
+rem        path and stores the result in a variable named <abs_path>.
+rem        This function fails if the <runfile_path> doesn't exist in mainifest
+rem        file.
+:: Start of rlocation
+goto :rlocation_end
+:rlocation
+if "%~2" equ "" (
+  echo>&2 ERROR: Expected two arguments for rlocation function.
+  exit 1
+)
+if "%RUNFILES_MANIFEST_ONLY%" neq "1" (
+  set %~2=%~1
+  exit /b 0
+)
+if "%RUNFILES_MANIFEST_FILE%" equ "" (
+  set RUNFILES_MANIFEST_FILE=%~f0.runfiles\MANIFEST
+)
+if not exist "%RUNFILES_MANIFEST_FILE%" (
+  set RUNFILES_MANIFEST_FILE=%~f0.runfiles_manifest
+)
+set MF=%RUNFILES_MANIFEST_FILE:/=\%
+if not exist "%MF%" (
+  echo>&2 ERROR: Manifest file %MF% does not exist.
+  exit 1
+)
+set runfile_path=%~1
+for /F "tokens=2* usebackq" %%i in (`%SYSTEMROOT%\system32\findstr.exe /l /c:"!runfile_path! " "%MF%"`) do (
+  set abs_path=%%i
+)
+if "!abs_path!" equ "" (
+  echo>&2 ERROR: !runfile_path! not found in runfiles manifest
+  exit 1
+)
+set %~2=!abs_path!
+exit /b 0
+:rlocation_end
+:: End of rlocation
+
+call :rlocation "io_bazel_rules_sass/sass/sass.sh" run_script
+for %%a in ("C:/Windows/system32/bash.exe") do set "bash_bin_dir=%%~dpa"
+set PATH=%bash_bin_dir%;%PATH%
+set args=%*
+rem Escape \ and * in args before passsing it with double quote
+if defined args (
+  set args=!args:\=\\\\!
+  set args=!args:"=\"!
+)
+"C:/Windows/system32/bash.exe" -c "!run_script! !args!"
